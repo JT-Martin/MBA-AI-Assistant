5f198b56b6a8641dc77276a23651ac5abf4ab4ba
Doooka <o3ean15@gmail.com>
2023-04-05 12:55:28 +1000

    set up repo

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: generatePrompt(req.body.audience, req.body.service, req.body.offer),
+    prompt: generatePrompt(req.body.input),
     temperature: 0.6,
     max_tokens: 2048
   });
@@ -16,15 +16,6 @@ export default async function (req, res) {
   res.status(200).json({ result: completion.data });
 }
 
-function generatePrompt(audience, service, offer) {
-  return `
-  Write 7 paragraphs of Facebook ad copy designed to sell a ${service} service.
-  Do not label the paragraphs by number. 
-  Talk about common problems people have if they're not using a ${service} service and try to minimize the amount of times you mention ${service}.
-  Write it at a 4th grade level. 
-  Write in a direct response style in the voice of a trusted consultant.
-  Direct it towards an audience of ${audience}.
-  Use a call to action of getting a ${offer}.
-  Do not start your response with a full stop or white space.
-  `;
+function generatePrompt(input) {
+  return `${input}`
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Head from "next/head";
 import { useState } from "react";
-import styles from "./index.module.css";
+import styles from "./styles.module.scss";
 
 export default function Home() {
-  const [audienceInput, setAudienceInput] = useState("");
-  const [serviceInput, setServiceInput] = useState("");
-  const [offerInput, setOfferInput] = useState("");
+  const [input, setInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
+    let button = document.getElementById("submit-button")
+    button.value = "Loading..."
     event.preventDefault();
     //console.log(JSON.stringify({ niche: nicheInput }));
     const response = await fetch("/api/generate", {
@@ -17,56 +17,108 @@ export default function Home() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-         audience: audienceInput,
-         service: serviceInput,
-         offer: offerInput 
+         input: input,
         }),
     });
     const data = await response.json();
     console.log(data)
     setResult(data.result.choices[0].text.trim())
+    button.value = "ASK YOUR Ai ASSISTANT"
   }
 
+  // async function copy() {
+  //   let textareaElement = document.getElementById("result");
+  //   let button = document.getElementById("copy-button")
+  //   // Select the text inside the textarea
+  //   //textareaElement.select();
+
+  //   // Copy the text to the clipboard
+  //   await navigator.clipboard.writeText(textareaElement.value);
+
+  //   button.innerHTML = "Copied!"
+  //   setTimeout(() => {
+  //     button.innerHTML = "Copy This Text"
+  //   }, "5000")
+  //}
+
   return (
-    <div>
+    <div className={styles.all}>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>MBA Ai Assistant</title>
+        <link rel="icon" href="/MBA PNG LOGO.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@400;700;900&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Passion+One&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Blinker&display=swap" rel="stylesheet" />
+
       </Head>
+      <header className={styles.header}>
+        <h3>MEET YOUR ARTIFICIAL INTELLIGENCE ASSISTANT</h3>
+        {/* <h4>Watch the below for instructions on how to use this A.I.</h4>
+        <button>Watch Explainer Video</button> */}
+      </header>
       <main className={styles.main}>
-        <h3>Facebook Ad Copy A.I. Helper</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="audience"
-            placeholder="Enter an audience... e.g. young professionals"
-            value={audienceInput}
-            onChange={(e) => setAudienceInput(e.target.value)}
-          />
-          <input
-            type="text"
-            name="service"
-            placeholder="Enter a service... e.g. buyers agent"
-            value={serviceInput}
-            onChange={(e) => setServiceInput(e.target.value)}
-          />
-          <input
-            type="text"
-            name="offer"
-            placeholder="Enter an offer... e.g. Free Property Buyers Checklist"
-            value={offerInput}
-            onChange={(e) => setOfferInput(e.target.value)}
-          />
-          <input type="submit" value="Generate ad copy" />
-        </form>
-        <div className={styles.output}>
-          <h4>Your Copy Suggestion:</h4>
-          <div className={styles.resultWrapper}>
-            <div className={styles.result}>{result}</div>
+        <video autoPlay={true} loop={true} muted={true} playsInline={true}>
+          <source src="https://player.vimeo.com/progressive_redirect/playback/781795783/rendition/540p/file.mp4?loc=external&signature=9da22a11712585ca4080f35dea5521544ce358a0a5155ef2d11fd0a3ebf85af8" type="video/mp4"/>
+        </video>
+        <div className={styles.grid}>
+          <form onSubmit={onSubmit} className={styles.input}>
+            <div>
+              <h3>Human</h3>
+              <h4>Input a detailed question, request or recipe and the Ai Assistant will respond to help.</h4>
+              <div className={styles.inputWrapper}>
+                <textarea
+                  type="text"
+                  rows={20}
+                  name="audience"
+                  placeholder={`Write Your Request Here
+
+                  The more detailed your request, the more specific the Ai will be in giving you a response.
+
+                  You can ask it to adapt it's responses by copying them and pasting them here with new instructions.`}
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                />
+              </div>
+            </div>
+            <input id="submit-button" type="submit" value="ASK YOUR Ai ASSISTANT" />
+          </form>
+          <div className={styles.output}>
+            <div>
+              <h3>Ai Assistant</h3>
+              <h4>Copy & Paste the response. Use it to enhance your ideas or ask it to make new versions.</h4>
+              <div className={styles.resultWrapper}>
+                <textarea 
+                    readOnly={true}
+                    type="text"
+                    rows={20}
+                    name="result"
+                    id="result"
+                    className={styles.result}
+                    placeholder={`A message from the Ai
+
+                    My suggestions should be improved on by you, the human.
+
+                    I am designed to be a smart tool to give you ideas, accelerate your work and help you with research.
+
+                    I am not a replacement for human intelligence.`}
+                    value={result}
+                />
+              </div>
+              {/* <div className={styles.revisionWrapper}>
+                <textarea
+                  type="text"
+                  rows={5}
+                  name="revisions"
+                  id="revisions"
+                  className
+                />
+              </div> */}
+              
+            </div>
+            {/* <button id="copy-button" onClick={copy}>Copy This Text</button> */}
           </div>
         </div>
       </main>
